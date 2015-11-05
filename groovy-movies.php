91a3c98a343e3eb23e76545de183d467046366f1
Patrick Martin <pmartin@crowdfaorite.com>
2015-11-05 14:51:37 -0700

    code review/comments

--- groovy-movies.php
+++ groovy-movies.php
@@ -1,25 +1,30 @@
 <?php
-/* Plugin Name: groovy-movies
+/*
+Plugin Name: groovy-movies
 Description:    Create a custom post type called Movies 
 Version:        1.0
 Author:         Patrick Martin
-*/     
-/**
- * Register a book post type.
- */
+*/
+
+// Register Movie Post Type
 function wpdocs_codex_custom_init() {
     $args = array(
         'public' => true,
         'label'  => __( 'Movies' ),
     );
+
     register_post_type( 'movies', $args );
 }
 add_action( 'init', 'wpdocs_codex_custom_init' );
+
+// Register 'genre' taxonomy
 function wpdocs_create_genre() {
-    register_taxonomy( 'genre', array('movies'), array(
-        'label'        => __( 'Genre'),
-        'rewrite'      => array( 'slug' => 'genre' ),
-        'hierarchical' => true )
-     );
+    register_taxonomy( 'genre', array( 'movies' ),
+        array(
+            'label' => __( 'Genre' ),
+            'rewrite' => array( 'slug' => 'genre' ),
+            'hierarchical' => true, 
+        )
+    );
 }
-add_action( 'init', 'wpdocs_create_genre', 0 );
+add_action( 'init', 'wpdocs_create_genre' );
